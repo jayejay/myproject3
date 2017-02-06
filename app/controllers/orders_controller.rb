8c27628e2ed4e1cd51f9341c6b5b593eea0031a7
Jay <julianeissing@web.de>
2017-02-06 20:00:52 +0100

    - selfmade orders_controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,9 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def landing_page
+    @featured_product = Product.first
+  end
+
 end
