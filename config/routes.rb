587e3fbd221cea040d0664143c6cf4f6830391ba
Jay <julianeissing@web.de>
2017-02-06 20:39:51 +0100

    added routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,19 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #root 'static_pages#index'
+
+  root 'static_pages#landing_page'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
+  root 'static_pages#landing_page'
+
 end
