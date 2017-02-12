88949bcefc8bde9fdf2f98c0ea9abc4da5379db2
JayBusiness <julianeissing@web.de>
2017-02-12 11:23:26 +0100

    5.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
-  resources :products
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
-
-  #root 'static_pages#index'
 
   root 'static_pages#landing_page'
+
+  resources :products
+
+  get 'static_pages/about', to: 'static_pages#about'
+
+  get 'static_pages/contact', to: 'static_pages#contact'
+
+  get 'static_pages/landing_page', to: 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
-
-  root 'static_pages#landing_page'
 
 end
