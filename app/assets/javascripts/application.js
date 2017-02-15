bd2cbb2ee3de4d32062d1b19ee3cfc64a1d400cf
Jay <julianeissing@web.de>
2017-02-15 15:53:20 +0100

    Exercize 5.8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).on('turbolinks:load',function(){
+    $('.product-column').matchHeight();
+});
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,5 @@
 
 $(document).on('turbolinks:load',function(){
-    $('.product-column').matchHeight();
 
     $('.alert').delay(2000).fadeOut(1500);
 
