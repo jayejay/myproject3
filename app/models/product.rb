2af8a580a904f0538f0fbcbcb64f63fc383b21f8
JayUbuntu <julianeissing@web.de>
2017-02-15 09:31:14 +0100

    Exercize 5.7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     if Rails.env.development?
@@ -7,6 +8,10 @@ class Product < ApplicationRecord
     else
       Product.where("name ILIKE ?", "%#{search_term}%")
     end
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
   end
 
 end
