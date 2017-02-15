36883c0c2b18797e300a7411e28d69e3f5e503b5
JayUbuntu <julianeissing@web.de>
2017-02-15 12:31:47 +0100

    Exercize 5.7

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :product
 
   scope :rating_desc, -> {order(rating: :desc)}
+  scope :rating_asc, -> {order(rating: :asc)}
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,5 +14,9 @@ class Product < ApplicationRecord
     comments.rating_desc.first
   end
 
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
 end
 
