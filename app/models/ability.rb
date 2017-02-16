1b7df33311187a0b9c32597a3bbdb3d65956f3f9
JayUbuntu <julianeissing@web.de>
2017-02-16 10:19:04 +0100

    Exercize 5.10

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can [:create, :read], Comment
+      can :read, Product
+    end
   end
 end
