384926dcee8a199d1097a8d8a8c2e9c4261c8d0e
jay <julianeissing@web.de>
2017-02-19 11:34:29 +0100

    Excercize 6.10

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,16 +1,18 @@
-
-$(document).on('turbolinks:load',function(){
-
-    $('.alert').delay(2000).fadeOut(1500);
-
-    $('.rating').raty({path: '/assets', scoreName:'comment[rating]'});
-
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
         readOnly: true,
         score: function() {
             return $(this).attr('data-score');
         }
     });
+};
+
+$(document).on('turbolinks:load ajaxSuccess',function(){
+
+    $('.alert').delay(2000).fadeOut(1500);
+
+    refreshRating();
 
     $('.image-zoom').elevateZoom({
         zoomType: 'lens',
