3890478d99b25631c18d0a674a38c2b1baf167f5
Jay <julianeissing@web.de>
2017-02-23 18:11:18 +0100

    - try to find search error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products.find(1)
+
     if params[:q]
       search_term = params[:q]
       @products = Product.search(search_term)
