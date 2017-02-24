dcfa000a91bf91124a866391575c6a6cad9731d1
JayUbuntu <julianeissing@web.de>
2017-02-24 15:21:30 +0100

    - added tests for products_controller_spec.rb

--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
 
-  sequence(:name) {|n|  "test_product_#{n}"}
+
   sequence(:description) {|n|  "description of test_product_#{n}"}
+  sequence(:name) {|n|  "test_product_#{n}"}
+  sequence(:id) {|n|  "#{n}"}
+
 
   factory :product do
+    id
     name
     description
     image_url "dvd.png"
